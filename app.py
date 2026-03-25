5349f90c39886fd0bcd796707300701cb645c4a2
pranjalikanda <your_email@example.com>
2026-03-25 20:02:01 +0530

    Initial commit with Jenkinsfile

--- app.py
+++ app.py
@@ -1,27 +1,11 @@
-import time
 from flask import Flask
-import mysql.connector
 
 app = Flask(__name__)
 
-# Retry connection until MySQL is ready
-for i in range(10):
-    try:
-        db = mysql.connector.connect(
-            host="db",
-            user="root",
-            password="password",
-            database="testdb"
-        )
-        print("✅ Connected to MySQL")
-        break
-    except:
-        print("⏳ Waiting for MySQL...")
-        time.sleep(3)
-
 @app.route('/')
 def home():
-    return "Flask + MySQL is working!"
+    return "CI/CD Pipeline Successful! The Flask App is Running."
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    # Hosted on 0.0.0.0 to be accessible within network/containers
+    app.run(host='0.0.0.0', port=5000)
